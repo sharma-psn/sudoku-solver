ccaaf62ff14a6b3d3995bf051a2720bfc285e673
sharma-psn <sandilya.narasimha007@gmail.com>
2026-08-18 07:18:38 +0530

    Module added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SudokuModule } from './sudoku/sudoku.module';
 import { ConfigModule } from '@nestjs/config';
+import { PythonModule } from './python-ocr/python.module';
 
 @Module({
-  imports: [SudokuModule,ConfigModule.forRoot({isGlobal: true})],
+  imports: [SudokuModule,ConfigModule.forRoot({isGlobal: true}), PythonModule],
   controllers: [AppController],
   providers: [AppService],
 })
